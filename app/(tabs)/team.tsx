9f9c1a299b541126a3544dc322341b0926e9a727
gbwilliamson4 <90010953+gbwilliamson4@users.noreply.github.com>
2024-09-22 20:25:07 -0600

    create team page

---
+++ app/(tabs)/team.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const team = () => {
+  return (
+    <View>
+      <Text>team</Text>
+    </View>
+  );
+};
+
+export default team;
